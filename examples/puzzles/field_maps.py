a9a78a7f7647199f552dd81c809c9729c316d480
Maxwell Brown <maxwellgbrown@gmail.com>
2019-05-09 16:42:21 -0400

    Only force inclusion on realistic possibilities

--- examples/puzzles/field_maps.py
+++ examples/puzzles/field_maps.py
@@ -52,6 +52,7 @@ SOURCE_FIELDS = [
     Field("SKU"),
     Field("Product Name"),
     Field("Vendor Name"),
+    Field("Duplicate"),
     Field("AAAAAAAAAA"),  # Should never map to anything
 ]
 
@@ -60,6 +61,7 @@ TARGET_FIELDS = [
     Field("sku"),
     Field("name"),
     Field("vendor"),
+    Field("Duplicate"),
     Field("ZZZZZZZZZZZ"),  # Nothing should have anything mapped to
 ]
 
@@ -75,19 +77,8 @@ def solve_maps(source_fields=SOURCE_FIELDS, target_fields=TARGET_FIELDS):
     """Attempt to map incoming fields to outgoing fields."""
     statement = list()
 
-    # Determine what source_fields can map to which target_fields
-    for target_field in target_fields:
-        statement += sat_utils.one_of(
-            str(Mapping(source_field, target_field))
-            for source_field in [*source_fields, NULL_FIELD]
-        )
-
-    # Determine what target_fields a source_field can map to
-    for source_field in source_fields:
-        statement += sat_utils.one_of(
-            str(Mapping(source_field, target_field))
-            for target_field in [*target_fields, NULL_FIELD]
-        )
+    source_possibilities = {f: {NULL_FIELD} for f in source_fields}
+    target_possibilities = {f: {NULL_FIELD} for f in target_fields}
 
     # TODO Perhaps writing a large DNF of possibilities for a single field for
     #      all evaluations is the way to go, instead of each of them at the top
@@ -96,12 +87,34 @@ def solve_maps(source_fields=SOURCE_FIELDS, target_fields=TARGET_FIELDS):
         # Fuzzy match fields by label name
         scores = fuzzywuzzy.process.extract(str(source_field), targets_by_label.keys())  # noqa
         log.debug("Scores for %s: %r", source_field, scores)
-        if any(score > 70 for _, score in scores):
+        passing_scores = [score for score in scores if score[1] > 70]
+        if passing_scores:
+            source_possibilities[source_field].update(
+                targets_by_label[label] for label, score in passing_scores
+            )
+            for label, _score in passing_scores:
+                target_field = targets_by_label[label]
+                target_possibilities[target_field].add(source_field)
+
             fuzzy_condition = sat_utils.from_dnf([
                 (str(Mapping(source_field, targets_by_label[target_label])),)
                 for target_label, score in scores if score > 70
             ])
             log.debug(fuzzy_condition)
             statement += fuzzy_condition
+
+    # Determine what source_fields can map to which target_fields
+    for target_field in target_fields:
+        statement += sat_utils.one_of(
+            str(Mapping(source_field, target_field))
+            for source_field in target_possibilities[target_field]
+        )
+
+    # Determine what target_fields a source_field can map to
+    for source_field in source_fields:
+        statement += sat_utils.one_of(
+            str(Mapping(source_field, target_field))
+            for target_field in source_possibilities[source_field]
+        )
 
     return statement
